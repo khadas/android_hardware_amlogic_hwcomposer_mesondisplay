36a13077f2028d4497f6f74dd42fb662eb7edf2d
Tianhua Sun <tianhua.sun@amlogic.com>
2023-11-16 13:53:48 +0800

    mesondisplay: refactor the uvm code [2/2]
    PD#SWPL-134890
    
    Problem:
    need refactor the uvm code
    
    Solution:
    move libuvm from hwc to mesondisplay
    
    Verify:
    ohm
    
    Change-Id: I54803a7135bad2e7326f35e17578448379f3ed59
    Signed-off-by: Tianhua Sun <tianhua.sun@amlogic.com>

---
+++ libs/libuvm/include/UvmDetach.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2021 Amlogic, Inc. All rights reserved.
+ *
+ * This source code is subject to the terms and conditions defined in the
+ * file 'LICENSE' which is part of this source code package.
+ *
+ * Description:
+ */
+
+#ifndef HWC_UVM_DETACH_H
+#define HWC_UVM_DETACH_H
+
+#include <deque>
+#include <hardware/hwcomposer2.h>
+
+#include <DrmSync.h>
+
+#include "UvmDev.h"
+
+class UvmDetach {
+public:
+    UvmDetach(hwc2_layer_t layerId);
+    UvmDetach(int tunnelId);
+    ~UvmDetach();
+
+    int32_t attachUvmBuffer(int bufferFd);
+    int32_t detachUvmBuffer();
+    int32_t collectUvmBuffer(const int fd, const int fence);
+    int32_t releaseUvmResource();
+    int32_t getVideoInfo(struct uvm_fd_info & videoInfo);
+
+    char *getDebugName() {return mName;};
+
+protected:
+    char mName[20];
+
+    struct UvmBuffer {
+        int bufferFd;
+        std::shared_ptr<DrmFence> releaseFence;
+    };
+
+    std::deque<UvmBuffer> mUvmBufferQueue;
+};
+#endif /* HWC_UVM_DETACH_H */
